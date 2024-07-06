a01a77c3aa252843c66c12e86c5c6998b4d16832
Everson-Aguilar <67832079+Everson-Aguilar@users.noreply.github.com>
2024-07-05 22:13:54 -0500

    subir

--- src/app/components/galeria_grid.jsx
+++ src/app/components/galeria_grid.jsx
@@ -1,40 +1,49 @@
+"use client";
+
 import Image from "next/image";
 import "../css/galeria.css"; // Asumiendo que este es tu archivo de estilos
 
-const MyComponent = () => {
-  // Array con las rutas de las imágenes
-  const images = [
-    "/DiseñoWeb/portfolio/g1.jpg",
-    "/DiseñoWeb/portfolio/g2.jpg",
-    "/DiseñoWeb/portfolio/g3.jpg",
-    "/DiseñoWeb/portfolio/g4.jpg",
-    "/DiseñoWeb/portfolio/g5.jpg",
-    "/DiseñoWeb/portfolio/g6.jpg",
-    "/DiseñoWeb/portfolio/g7.jpg",
-    "/DiseñoWeb/portfolio/g8.jpg",
-    "/DiseñoWeb/portfolio/g9.jpg",
-    "/DiseñoWeb/portfolio/g10.jpg",
-    "/DiseñoWeb/portfolio/g11.jpg",
-    "/DiseñoWeb/portfolio/g12.jpg",
-  ];
+const images = [
+  "/DiseñoWeb/portfolio/g1.jpg",
+  "/DiseñoWeb/portfolio/g2.jpg",
+  "/DiseñoWeb/portfolio/g3.jpg",
+  "/DiseñoWeb/portfolio/g4.jpg",
+  "/DiseñoWeb/portfolio/g5.jpg",
+  "/DiseñoWeb/portfolio/g6.jpg",
+  "/DiseñoWeb/portfolio/g7.jpg",
+  "/DiseñoWeb/portfolio/g8.jpg",
+  "/DiseñoWeb/portfolio/g9.jpg",
+  "/DiseñoWeb/portfolio/g10.jpg",
+  "/DiseñoWeb/portfolio/g11.jpg",
+  "/DiseñoWeb/portfolio/g12.jpg",
+];
+
+const galeria = () => {
+  // Función para manejar el clic en el botón
+  const handleClick = (id) => {
+    console.log(`Se hizo clic en el botón ${id}`);
+  };
+
+  // Generar los IDs de los botones
+  const ids = images.map((_, index) => `b${index + 1}`);
 
   return (
     <div className="galeria w-full h-full grid grid-cols-4 gap-4">
-      {/* Mapeamos sobre el array de imágenes */}
+      {/* Renderizamos cada imagen de manera individual */}
       {images.map((image, index) => (
         <div key={index} className="relative rounded-lg overflow-hidden">
-          <Image
-            src={image}
-            alt={`Imagen ${index + 1}`}
-            width={400}
-            height={400}
-          />
-          <button className="absolute h-5 w-auto top-2 right-2  bg-neutral-800 text-black px-3 py-1 rounded-md shadow-md">
+          <Image src={image} alt={`Imagen ${index + 1}`} width={400} height={400} />
+          <button
+            key={index}
+            id={ids[index]}
+            className="absolute h-5 w-auto top-2 right-2 bg-neutral-800 text-black px-3 py-1 rounded-md shadow-md"
+            onClick={() => handleClick(ids[index])} // Manejador de evento onClick
+          >
             <Image
               src="/DiseñoWeb/ver.svg"
               alt="ver"
-              layout="fill" // Ajusta la imagen para llenar todo el contenedor
-              objectFit="fill" // Ajusta la imagen para llenar completamente el contenedor, sin recortar
+              layout="fill"
+              objectFit="fill"
             />
           </button>
         </div>
@@ -43,4 +52,4 @@ const MyComponent = () => {
   );
 };
 
-export default MyComponent;
+export default galeria;
