bed7152d3042c39c3f979f15db0e5e7a5d58524d
Everson-Aguilar <67832079+Everson-Aguilar@users.noreply.github.com>
2024-07-23 04:40:59 -0500

    subir

--- src/app/biblioteca/layout.jsx
+++ src/app/biblioteca/layout.jsx
@@ -4,17 +4,18 @@ import Header from "../components/header";
 
 const Layout = ({ children }) => {
   return (
-    <div className="relative bg-neutral-600 min-h-screen box pr-3 pl-3 ">
+    <div className="relative bg-neutral-600 min-h-screen box  ">
       
-      {/* Encabezado */}
+    
 
       <header>
         <Header />
       </header>
 
       {/* Espacio para el contenido */}
-      <main className="pt-8">{children}</main>
-      <div className="bg-neutral-600 h-3"></div>
+
+      <main>{children}</main>
+      <div className=   "bg-neutral-600 h-3 "></div>
     </div>
   );
 };
