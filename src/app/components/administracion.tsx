31fa62fb14e19613e6ffc08161874d2dc7148844
Everson-Aguilar <67832079+Everson-Aguilar@users.noreply.github.com>
2024-07-23 03:31:59 -0500

    subir

--- src/app/components/administracion.tsx
+++ src/app/components/administracion.tsx
@@ -2,6 +2,11 @@ import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import credentials from 'admin.json'; // Ajusta la ruta según tu estructura de carpetas
 
+
+// css
+
+import "../css/header.css";
+
 const Login: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -34,36 +39,47 @@ const Login: React.FC = () => {
         setError('Error al conectar con el servidor');
       }
     } else {
-      setError('Usuario o contraseña incorrectos');
+      setError('credenciales incorrectas');
     }
   };
 
   return (
-    <div>
-      <h2>Iniciar sesión</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Usuario:</label>
-          <input
-            id="username"
+    <div className='relative p-1  flex  overflow-scroll justify-end text-sm font-Quicksand text-neutral-300 items-start h-full  bg-neutral-600 border-b border-neutral-400'>
+      <form className='  sesion_mobil  flex-row  items-center justify-center mr-28' onSubmit={handleSubmit}>
+
+        
+
+
+
+      
+        
+          <label className=' mr-7  mb-1 '  htmlFor="username">Usuario:</label>
+          <input className=' border h-5  mb-1 w-40 rounded-lg p-1  mr-5'
+            id="username" 
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
           />
-        </div>
-        <div>
-          <label htmlFor="password">Contraseña:</label>
-          <input
+        
+        <div > 
+
+          <label className='  mr-1 mb-1 ' htmlFor="password">Contraseña:</label>
+          <input  className='  border  mb-1  h-5  w-40 rounded-lg p-1  mr-5'
             id="password"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-        </div>
+        
         {error && <p>{error}</p>}
-        <button type="submit">Iniciar sesión</button>
+
+        
+        
+        </div>
+        <div>  <button className='sesion'   type="submit">Iniciar sesión</button> </div>
+
       </form>
     </div>
   );
