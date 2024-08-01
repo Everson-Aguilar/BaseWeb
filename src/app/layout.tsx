d15a662a4cb09afef20ad968004e1c66b14ede9d
Everson-Aguilar <67832079+Everson-Aguilar@users.noreply.github.com>
2024-08-01 02:48:29 -0500

    subir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -12,11 +11,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/DiseñoWeb/logogrisoscuro.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/DiseñoWeb/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/DiseñoWeb/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/DiseñoWeb/favicon-16x16.png" />
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
+        <meta name="keywords" content="diseño 3D, modelado 3D, renderizado, visualización arquitectónica, diseño de interiores, animación 3D, simulaciones 3D" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
