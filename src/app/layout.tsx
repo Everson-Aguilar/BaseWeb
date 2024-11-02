c8de6b3ccfa7ad8e2a88e1d98dbcd0b63956c73a
Everson-Aguilar <67832079+Everson-Aguilar@users.noreply.github.com>
2024-11-01 22:31:01 -0500

    01/11/2024

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,9 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title:
-    "Creamos Negocios Digitales con Redes Sociales, Marketing, Web, Orientación Artística y Obtención de Clientes",
+  title: "Marketing Digital y Contenido 3D para Impulsar tu Negocio",
   description:
-    "En tudigitalnegocio.com, transformamos tu negocio local en un poderoso negocio digital que impulsa tus ventas. Utilizamos redes sociales, marketing digital, desarrollo web, diseño gráfico, diseño de productos y diseño de espacios. Con tecnologías avanzadas y un flujo de trabajo optimizado, ofrecemos soluciones económicas y adaptadas a tus necesidades. ¡Conéctate con nosotros y da el primer paso hacia el éxito digital!",
+  "Impulsa tu negocio con Marketing Digital y visualizaciones 3D impactantes. En tudigitalnegocio.com, mejoramos la experiencia de compra de tus clientes con soluciones innovadoras. ¡Haz que tus ideas cobren vida!",
 };
 
 export default function RootLayout({
