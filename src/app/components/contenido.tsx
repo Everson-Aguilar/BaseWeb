455fac4e93836da5416dcfca18df915a765c5e9a
Everson-Aguilar <67832079+Everson-Aguilar@users.noreply.github.com>
2024-11-27 16:46:52 -0500

    27/11/2024

--- src/app/components/contenido.tsx
+++ src/app/components/contenido.tsx
@@ -9,6 +9,9 @@ interface Content {
   ContentStatus:string;
 }
 
+
+
+
 const contentList: Content[] = [
   {
     title: "The Art of Survival",
@@ -32,9 +35,19 @@ const ContentContainer: React.FC = () => {
 
     <section>
 
-    <h2 className=" text-4xl flex flex-row justify-center items-center p-5 font-BebasNeue text-trend">Chapters</h2>
+        <section className="    flex-row justify-center items-center p-5  text-trend">
 
-    
+    <h2 className=" text-4xl font-BebasNeue  ">Chapters</h2>
+
+    <h3> <strong>Form Follows Function – Louis Sullivan</strong></h3>
+
+    <p> <span className="text-trend text-5xl ">!</span>A firm believer that functionality is what shapes design! I believe that something truly functional feels alive, resonates with us, reduces unnecessary effort, and serves a clear purpose. That’s why every chapter of my experience and exploration is focused on crafting solutions that are not only practical but also aesthetically meaningful.
+
+My approach blends forms, colors, textures, and purpose, ensuring that each element has a coherent meaning, is practical, enjoyable, and adds unique value. Every design I create is born from logic, from understanding the why behind things, embracing their essence to build authentic solutions.
+
+I strive to see reality at its brightest, appreciating what is truly meaningful while avoiding getting lost in the distortion of superficiality. My goal is to create transcendent experiences where functionality and design converge to illuminate clear and purposeful paths.</p>
+
+</section>
 
     <section className="grid gap-8 px-6 py-12 lg:grid-cols-2 max-w-6xl mx-auto">
       {contentList.map((content, index) => (
