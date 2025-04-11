b0bb5b9d152e90a656396edda8c167be12637bf2
Everson-Aguilar <67832079+Everson-Aguilar@users.noreply.github.com>
2025-04-10 23:35:51 -0500

    10/04/2025

--- src/app/pages/freelancer/page.tsx
+++ src/app/pages/freelancer/page.tsx
@@ -1,27 +1,28 @@
-//componente importado
-
 "use client";
 
-
+import dynamic from "next/dynamic";
 import ProfileFreelancer from "../../components/profile";
-import Session_expired from "../../components/session_expired";
+
+// Importa Session_expired dinámicamente para evitar problemas con sessionStorage
+const Session_expired = dynamic(() => import("../../components/session_expired"), {
+  ssr: false,
+});
 
 const Profile = () => {
   return (
     <>
       <section>
-        {" "}
         <Session_expired />
       </section>
 
-      <section className=" md:flex flex-row   h-full">
-        <section className="  w-full md:w-1/3  ">
+      <section className="md:flex flex-row h-full">
+        <section className="w-full md:w-1/3">
           <ProfileFreelancer />
         </section>
       </section>
 
       <section className="text-trend p-5">
-        <h2 className="text-2xl font-BebasNeue  ">Actualización</h2>
+        <h2 className="text-2xl font-BebasNeue">Actualización</h2>
         <p>
           Se estarán generando nuevas tareas de forma continua para fomentar la
           colaboración dentro del sistema. De esta manera, podremos construir un
